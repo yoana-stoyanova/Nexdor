f2ca511c1e0afe9cdfaaabbdb1c085a476e3b3c1
yoana stoyanova <82516108+yoana-stoyanova@users.noreply.github.com>
2025-04-04 22:48:14 +0300

    Update enter-apt.js

--- js/apt-settings/enter-apt.js
+++ js/apt-settings/enter-apt.js
@@ -1,16 +1,15 @@
 let block = JSON.parse(localStorage.getItem('block'));
 let aptId = localStorage.getItem('apt-id');
 
+console.log(block, aptId)
 document.addEventListener("DOMContentLoaded", function () {
     let passwordField = document.getElementById('password');
     let btn = document.querySelector('button');
-    
-    passwordField.value = 'MdkEvT';
 
     btn.addEventListener('click', function() {
         if(passwordField.value.trim() == '') return;
          
-        let apt = block['apartments'][aptId - 1];
+        let apt = block['apartments'][Number(aptId) - 1];
 
         if(passwordField.value != apt.password) return;
 
