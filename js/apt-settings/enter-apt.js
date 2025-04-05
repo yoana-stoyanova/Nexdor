d48a7c7611f85e11391cf08c61cb17ac5a226716
yoana stoyanova <jstoyanova14.05@gmail.com>
2025-04-05 11:49:51 +0300

    aptId2

--- js/apt-settings/enter-apt.js
+++ js/apt-settings/enter-apt.js
@@ -1,19 +1,23 @@
 let block = JSON.parse(localStorage.getItem('block'));
 
-console.log(block, aptId)
 document.addEventListener("DOMContentLoaded", function () {
     let passwordField = document.getElementById('password');
     let btn = document.querySelector('button');
 
     btn.addEventListener('click', function() {
-        if(passwordField.value.trim() == '') return;
         let aptId = localStorage.getItem('apt-id');
-         
+
+        if(passwordField.value.trim() == '') return;
+        
         let apt = block['apartments'][Number(aptId) - 1];
 
         console.log(Number(aptId) - 1, apt);
         
-        if(passwordField.value != apt.password) return;
+        if(passwordField.value != apt.password) {
+            console.log(passwordField.value, apt.password);
+            
+            return;
+        }
 
         localStorage.setItem('apt', JSON.stringify(apt));
         
